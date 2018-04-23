31e760c22ca992efcb74b5779e5683b930fe3c4a
poorvitusam <poorvitusam@gmail.com>
2018-04-23 00:02:53 -0400

    Implemented charts to display the weather forecast

---
+++ src/containers/weather_list.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
+
+class WeatherList extends Component {
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temperatures = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
+        return(
+            <tr key={name}>
+                <td>{name}</td>
+                <td><Chart data={pressures} color="green" unit="hPa"/></td>
+                <td><Chart data={humidities} color="blue" unit="%"/></td>
+                <td><Chart data={temperatures} color="orange" unit="K"/></td>
+            </tr>
+        );
+    }
+
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+function mapStateToProps({weather}) {
+    return {weather};
+}
+export default connect(mapStateToProps)(WeatherList);
